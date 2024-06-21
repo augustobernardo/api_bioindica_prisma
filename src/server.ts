585be4ab7aab8ed8d2066c8686f33e8b3032481e
Augusto Oliveira <gutooliveira027@gmail.com>
2024-06-21 08:39:20 -0300

    chore: Update server.ts to use port 8080
    The code changes in `server.ts` update the server to use port 8080 instead of the previous port 3000. This change ensures that the server listens on the correct port.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`chore` for a maintenance task) and provides a clear and concise description of the purpose of the code changes.

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,14 @@ import express from 'express';
 import { routes } from './routes';
 
 const app = express();
-const port = 3000;
+const port = 8080;
+
+app.use((req, res) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+})
+
 
 app.use(express.json());
 app.use(routes);
