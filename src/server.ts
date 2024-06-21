6f499ab3b48b3544e8e7b9f802164b9ade920c0c
Augusto Oliveira <gutooliveira027@gmail.com>
2024-06-21 09:16:41 -0300

    chore: Update server.ts to use dynamic port and add CORS support
    The code changes in `server.ts` update the server to use a dynamic port instead of relying on the `process.env.PORT` variable. This change allows for more flexibility in specifying the port number. Additionally, CORS support has been added to allow requests from the specified `HOSTNAME`.
    
    This commit message follows the established convention of using a prefix to indicate the type of change (`chore` for a maintenance task) and provides a clear and concise description of the purpose of the code changes.

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import { routes } from './routes';
 
 const app = express();
-const port = 8080;
-
-app.use((req, res) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.header('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
-})
-
+const PORT = process.env.PORT || 3000;
+const HOSTNAME = process.env.HOSTNAME || 'http://localhost';
 
 app.use(express.json());
 app.use(routes);
 
-app.listen(port, () => console.log(`Server is running on port ${port}` ));
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+});
+
+app.use(cors({
+    origin: [`${HOSTNAME}`]
+}))
+
+app.listen(PORT, () => {
+    console.log(`Server is running on ${HOSTNAME}:${PORT}`);
+});
